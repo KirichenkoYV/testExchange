08779c419793c7959887491890aaffb273a40753
KirichenkoYV <hause99@gmail.com>
2023-03-15 23:07:26 +0300

    createHTML: done.

--- testExchange/src/pages/exchangePage/ExchangePage.tsx
+++ testExchange/src/pages/exchangePage/ExchangePage.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 import style from "ExchangePage.module.scss";
 
 function ExchangePage() {
-  return <div>
-    sdadasdas
-  </div>;
+
+    
+  const [addressEth, setAdressEth] = useState<string>("");
+
+  function changeAdressEth(event: React.ChangeEvent<HTMLInputElement>): void {
+    setAdressEth(event?.target.value);
+  }
+
+  return (
+    <div>
+      Crypto Exchange
+      <div>Exchange fast and easy</div>
+      <input></input>
+      <input></input>
+      <div>Your Ethereum address</div>
+      <input value={addressEth} onChange={changeAdressEth}></input>
+      <button>Exchange</button>
+    </div>
+  );
 }
 
 export default ExchangePage;
